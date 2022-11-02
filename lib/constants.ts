4d5e1df0f4fa6b99d2dd5ad968b1b703a5407402
prosoxwb <cyberstudent115@gmail.com>
2022-11-02 22:42:53 +0700

    add files via upload

--- lib/constants.ts
+++ lib/constants.ts
@@ -17,16 +17,16 @@
 export const SITE_URL = 'https://demo.vercel.events';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
 export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const BRAND_NAME = 'IMK';
+export const SITE_NAME_MULTILINE = ['IMK', 'Tubes'];
+export const SITE_NAME = 'Tubes IMK';
 export const META_DESCRIPTION =
-  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
+  'Website untuk memenuhi tugas besar mata kuliah IMK';
 export const SITE_DESCRIPTION =
   'An interactive online experience by the community, free for everyone.';
-export const DATE = '01 January 2022';
-export const SHORT_DATE = 'Jan 1 - 9:00am PST';
-export const FULL_DATE = 'Jan 1st 9am Pacific Time (GMT-7)';
+export const DATE = '17 August 1945';
+export const SHORT_DATE = 'Aug 17 - 9:00am PST';
+export const FULL_DATE = 'Aug 17th 9am Pacific Time (GMT+7)';
 export const TWEET_TEXT = META_DESCRIPTION;
 export const COOKIE = 'user-id';
 
